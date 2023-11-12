c3368eb5ecbecbfffa892006877a27aea65e3eee
skkamalesh <kamalesh0326@gmail.com>
2023-11-13 00:30:14 +0530

    completed

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://kamalesh0326:kamalesh26072K@cluster0.yvlch6j.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
